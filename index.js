b38bcbb660eb900dbedb9c378addf7fafcfa3686
Baktash <Baktash.Ahmed@mergermarket.com>
2018-04-25 22:31:33 +0100

    sorting now

---
+++ index.js
@@ -0,0 +1,20 @@
+const _ = require('lodash');
+
+
+const sort = (obj) => {
+    if (_.isPlainObject(obj)) {
+        return _(obj).keys().sortBy().reduce((o, i) => {
+            o[i] = sort(obj[i]);
+            return o;
+        }, {});
+    }
+
+    if (_.isArray(obj)) {
+        return _(obj).sortBy().map(o => sort(o)).value();
+    }
+
+    return obj;
+};
+
+
+module.exports = sort;
